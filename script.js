5e735ac7bfa3fd17c1d9a9179d303f54a796f515
somildholakia <somildholakia0852@gmail.com>
2025-12-30 18:33:47 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let change_text = document.getElementById("main-container")
 let paragraph = document.getElementById("text")
-change_text.addEventListener("click",function(){
+change_text.addEventListener("click", function () {
     paragraph.textContent = "I want to open the box"
 })
