2f12e95345189119c0f9f52874fd3d2db8441793
somildholakia <somildholakia0852@gmail.com>
2025-12-30 19:39:16 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let change_text = document.getElementById("main-container")
 let paragraph = document.getElementById("text")
 change_text.addEventListener("click", function () {
-    paragraph.textContent = "I want to open the box"
+   change_text.innerHTML = "<button onclick='buy()'> BUY! </button><br>"
 })
+
+
+function buy(){
+    change_text.innerHTML = "<p> Thank you for buyings</p>"
+    console.log("purchased")
+}
