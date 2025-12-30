851501450873580fa2523a1d7b257a5df9b7ebdb
somildholakia <somildholakia0852@gmail.com>
2025-12-30 19:59:37 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let change_text = document.getElementById("main-container")
 let paragraph = document.getElementById("text")
 change_text.addEventListener("click", function () {
-   change_text.innerHTML = "<button onclick='buy()'> BUY! </button><br>"
+   change_text.innerHTML += "<button onclick='buy()'> BUY! </button><br>"
 })
 
 
 function buy(){
-    change_text.innerHTML = "<p> Thank you for buyings</p>"
+    change_text.innerHTML += "<p> Thank you for buyings</p>"
     console.log("purchased")
 }
