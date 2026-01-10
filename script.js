6fd35e8a334336a8707fef567fc0e5fdbbe7cacc
somildholakia <somildholakia0852@gmail.com>
2026-01-10 13:08:57 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-fetch('https://dog.ceo/api/breeds/image/random')
-.then(Response => {
-    return Response.json()
-})
-.then(data => {
-    const imageElement = document.createElement('img')
-    imageElement.src = data.message
-    imageElement.alt = "Random dog photo"
-    document.getElementById("main-container").appendChild(imageElement)
-})
+
+async function getApi(){
+   const response = await fetch('https://apis.scrimba.com/bored/api/activity')
+   const data = await response.json()
+   console.log(data)
+}
+
+getApi()
