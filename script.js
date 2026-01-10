8e0a3615648aa928b20cacd0451b522c2a69412e
somildholakia <somildholakia0852@gmail.com>
2026-01-10 14:10:26 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 async function getApi(){
     try {
-    const response = await fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    const response = await fetch("https://apis.scrimba.com/jsonplaceholder/posts",
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'holiday nightmares',
+                body: 'When i was Kidnapped in scotland',
+                id: 100
+            })
+        })
 
     if(!response.ok){
         throw new Error(`Error occured: ${response.status}`)
