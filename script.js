382c8991bff2a100bbb7435e1765882717e0ed19
somildholakia <somildholakia0852@gmail.com>
2026-01-11 12:58:43 +0530

    update

---
+++ script.js
@@ -0,0 +1,33 @@
+async function getApi(url) {
+
+    const response = await fetch(url)
+
+    if (!response.ok) {
+        throw new Error("Endpoint Error")
+    }
+
+    const data = await response.json()
+
+    return new Promise((resolve, reject) => {
+        const newImg = new Image()
+        newImg.src = data.message
+        newImg.alt = "A dog Photo"
+
+        newImg.addEventListener("load", () => {
+            resolve(newImg)
+        })
+
+        newImg.addEventListener("error", () => {
+            reject("Error")
+        })
+    })
+}
+
+
+
+try {
+    const ImageEl = await getApi("https://dog.ceo/api/breeds/image/random")
+    document.getElementById("main-container").appendChild(ImageEl)
+} catch (err) {
+    console.log(err)
+}
