5a4d2b8d72c1a5ea3fe145c6e5ac7e0f6cc09db9
somildholakia <somildholakia0852@gmail.com>
2026-01-11 13:07:26 +0530

    update

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-async function getApi(url) {
+async function getApi(url){
 
     const response = await fetch(url)
 
-    if (!response.ok) {
+    if(!response.ok){
         throw new Error("Endpoint Error")
     }
 
     const data = await response.json()
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve,reject) => {
         const newImg = new Image()
         newImg.src = data.message
         newImg.alt = "A dog Photo"
 
-        newImg.addEventListener("load", () => {
+        newImg.addEventListener("load",() => {
             resolve(newImg)
         })
 
-        newImg.addEventListener("error", () => {
-            reject("Error")
+        newImg.addEventListener("error", ()=> {
+            reject("Error Occured")
         })
     })
+
 }
 
 
-
 try {
+
     const ImageEl = await getApi("https://dog.ceo/api/breeds/image/random")
     document.getElementById("main-container").appendChild(ImageEl)
-} catch (err) {
+
+} catch(err) {
     console.log(err)
 }
